cfe25234c21b812dd1e3eca588cfb012b8c64603
Alysson Kelvim <akcsalysson@gmail.com>
2020-07-11 11:31:42 -0300

    Interceptor para incluir token nas requisições

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -7,7 +7,7 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS
 export class ErrorInterceptor implements HttpInterceptor{
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        console.log("Interceptor");
+        
         return next.handle(req)
         .catch((error, caught)=> {
             let errorObj = error;
